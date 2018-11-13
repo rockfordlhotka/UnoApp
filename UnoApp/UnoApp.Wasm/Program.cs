3287a312928dd62311aaa05f73765ab216dcc4a7
Rockford Lhotka <rocky@lhotka.net>
2018-11-12 22:25:50 -0600

    Add project files.

---
+++ UnoApp/UnoApp.Wasm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Windows.UI.Xaml;
+
+namespace UnoApp.Wasm
+{
+  public class Program
+  {
+    private static App _app;
+
+    static void Main(string[] args)
+    {
+      Windows.UI.Xaml.Application.Start(_ => _app = new App());
+    }
+  }
+}
